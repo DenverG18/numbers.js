0e75fa11ff26d9bcc0b716bc1cc388cddb783836
Your Name <you@example.com>
2024-09-13 15:06:13 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const favoriteNumber1 = 24;
+const favoriteNumber2 = 18;
+
+const sum = favoriteNumber1 + favoriteNumber2;
+console.log(sum);
+
+const  difference = favoriteNumber1 - favoriteNumber2;
+console.log(difference);
+
+const product = favoriteNumber1 * favoriteNumber2;
+console.log(product);
+
+const quotient = favoriteNumber1 / favoriteNumber2;
+console.log(quotient);
+
+const modulus = favoriteNumber1 % favoriteNumber1;
+console.log(modulus);
+
+const power = favoriteNumber1 ** favoriteNumber2;
+console.log(power);
